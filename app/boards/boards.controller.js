fb48a33b3ca81941f88dde5022da6d4c43fb67c4
OOOX <oooxdesign@ex.ua>
2016-03-10 13:34:09 +0200

    dz #5

---
+++ app/boards/boards.controller.js
@@ -0,0 +1,76 @@
+(function() {
+	'use strict';
+
+	function BoardsController(BoardsService, $log) {
+		$log.debug('BoardsController');
+
+		var vm = this,
+			_defauts = {
+				title: '',
+				author: '',
+				description: '',
+				isPublic: false,
+				notes: []
+			};
+		vm.newBoard = angular.copy(_defauts);
+		vm.boards = [];
+		vm.remove = _remove;
+		vm.add = _add;
+
+		function _showError(error) {
+				$log.error(error);
+		}
+		function _updateBoardModel() {
+			// метод getBoards сервиса BoardsService возвращает Promise объект
+			var promiseFromBoard = BoardsService.find();
+
+			// добавляем функции обратного вызова
+			promiseFromBoard.then(function(result){
+				$log.debug(result);
+				// http://joxi.ru/Y2LDBgESnqZd62
+				// в result мы получаем:
+				// result.config - то что было в запросе
+				// result.data - непосредственно данные, которые отдал сервер
+				// result.header - заголовок ответа сервера
+				// result.status - статус ответа сервера
+				// result.statusText - текст статуса
+
+				// изменяем нашу модель приложения
+				vm.boards = result.data;
+			}).catch(_showError);
+		}
+		function _add(board, boardForm) {
+			BoardsService
+				.create(board.title, board.description, board.isPublic)
+				.then(function() {
+					_updateBoardModel();
+					_reset(boardForm);
+				})
+				.catch(_showError);
+		}
+		function _remove(boardId) {
+			BoardsService.destroy(boardId)
+				.then(_updateBoardModel)
+				.catch(_showError);
+		}
+
+		function _reset(form) {
+			$log.debug(form);
+
+			if (form) {
+				form.$setPristine();
+				form.$setUntouched();
+			}
+			vm.newBoard = angular.copy(_defauts);
+		}
+
+		_updateBoardModel();
+	}
+	BoardsController.$inject = ['BoardsService', '$log'];
+
+	angular
+		.module('noteriousApp')
+		.controller('BoardsController', BoardsController);
+
+
+})();
---
+++ app/notes/notes.controller.js
@@ -0,0 +1,44 @@
+(function() {
+	'use strict';
+
+	function NotesController(BoardsService, $routeParams, $log, $location) {
+		$log.debug('NotesController');
+		$log.debug('$routeParams', $routeParams);
+
+		var vm = this,
+			defauts = {
+				title: '',
+				content: '',
+				tags: []
+			};
+		vm.board = BoardsService.getByIndex($routeParams.id);
+		$log.debug('board: ', vm.board);
+		if (!vm.board) {
+			$location.path( "/" );
+			$log.debug('board is undefined, so redirect to "/"');
+		}
+		vm.add = function add(note) {
+			BoardsService.addNote($routeParams.id, note);
+			_reset();
+		};
+		vm.remove = function remove(note) {
+			BoardsService.removeNote($routeParams.id, note);
+		};
+
+		vm.note = angular.copy(defauts);
+		vm.add = function add(note) {
+			BoardsService.addNote($routeParams.id, note);
+			_reset();
+		};
+		function _reset() {
+			vm.note = angular.copy(defauts);
+		}
+	}
+	NotesController.$inject = ['BoardsService', '$routeParams', '$log', '$location'];
+
+	angular
+		.module('noteriousApp')
+		.controller('NotesController', NotesController);
+
+
+})();
